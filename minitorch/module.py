5afd4cdd8b746302f0d1192d9f6a1b83c1ba4245
uGAliok <gagarina.u@gmail.com>
2026-09-14 14:23:06 +0300

    Minitorch module 0

--- minitorch/module.py
+++ minitorch/module.py
@@ -31,13 +31,15 @@ class Module:
 
     def train(self) -> None:
         """Set the mode of this module and all descendent modules to `train`."""
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        self.training = True
+        for module in self.modules():
+            module.train()
 
     def eval(self) -> None:
         """Set the mode of this module and all descendent modules to `eval`."""
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        self.training = False
+        for module in self.modules():
+            module.eval()
 
     def named_parameters(self) -> Sequence[Tuple[str, Parameter]]:
         """Collect all the parameters of this module and its descendents.
@@ -47,13 +49,15 @@ class Module:
             The name and `Parameter` of each ancestor parameter.
 
         """
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        parameters = list(self._parameters.items())
+        for module_name, module in self._modules.items():
+            for parameter_name, parameter in module.named_parameters():
+                parameters.append((f"{module_name}.{parameter_name}", parameter))
+        return parameters
 
     def parameters(self) -> Sequence[Parameter]:
         """Enumerate over all the parameters of this module and its descendents."""
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        return [parameter for _, parameter in self.named_parameters()]
 
     def add_parameter(self, k: str, v: Any) -> Parameter:
         """Manually add a parameter. Useful helper for scalar parameters.
--- minitorch/operators.py
+++ minitorch/operators.py
@@ -32,7 +32,71 @@ from typing import Callable, Iterable
 # $f(x) = |x - y| < 1e-2$
 
 
-# TODO: Implement for Task 0.1.
+def mul(x: float, y: float):
+    return x * y
+
+
+def id(x: float):
+    return x
+
+
+def add(x: float, y: float):
+    return x + y
+
+
+def neg(x: float):
+    return -x
+
+
+def lt(x: float, y: float):
+    return 1.0 if x < y else 0.0
+
+
+def eq(x: float, y: float):
+    return 1.0 if x == y else 0.0
+
+
+def max(x: float, y: float):
+    return x if x > y else y
+
+
+def is_close(x: float, y: float) -> bool:
+    return abs(x - y) < 1e-2
+
+
+def sigmoid(x: float):
+    if x >= 0:
+        return 1.0 / (1.0 + math.exp(-x))
+    exp_x = math.exp(x)
+    return exp_x / (1.0 + exp_x)
+
+
+def relu(x: float):
+    return x if x > 0 else 0.0
+
+
+def log(x: float):
+    return math.log(x)
+
+
+def exp(x: float):
+    return math.exp(x)
+
+
+def inv(x: float):
+    return 1.0 / x
+
+
+def log_back(x: float, d: float):
+    return d / x
+
+
+def inv_back(x: float, d: float):
+    return -d / (x * x)
+
+
+def relu_back(x: float, d: float):
+    return d if x > 0 else 0.0
 
 
 # ## Task 0.3
@@ -52,3 +116,38 @@ from typing import Callable, Iterable
 
 
 # TODO: Implement for Task 0.3.
+def map(func):
+    def map_function(iter: Iterable):
+        ans = []
+        for i in iter:
+            ans.append(func(i))
+        return ans
+    return map_function
+
+def zipWith(func):
+    def zip_function(x: Iterable, y: Iterable):
+        c = []
+        for a, b in zip(x, y):
+            c.append(func(a, b))
+        return c
+    return zip_function
+
+def reduce(func, start):
+    def reduce_function(iter: Iterable):
+        acc = start
+        for i in iter:
+            acc = func(acc, i)
+        return acc
+    return reduce_function
+
+def negList(iter):
+    return map(neg)(iter)
+
+def addLists(x, y):
+    return zipWith(add)(x, y)
+
+def sum(iter):
+    return reduce(add, 0.0)(iter)
+
+def prod(iter):
+    return reduce(mul, 1.0)(iter)
